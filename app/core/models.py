9da981087c058ba36d8131e6f7c459b6038c6878
PythonicAccountant <44148347+PythonicAccountant@users.noreply.github.com>
2020-04-16 00:53:23 -0700

    added superuser func

--- app/core/models.py
+++ app/core/models.py
@@ -5,8 +5,18 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """ Creates and saves a new user """
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("Users must have emails")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_super_user(self, email, password=None):
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self.db)
         return user
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,3 +10,28 @@ class model_tests(TestCase):
         user = get_user_model().objects.create_user(email=email, password=password,)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ Test the email for a new user is normalized """
+
+        email = "test@PYTHONaccounting.com"
+        password = "Testpass123"
+        user = get_user_model().objects.create_user(email=email, password=password,)
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ Test creating user with no email raises error """
+        email = ""
+        password = "Testpass123"
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email=email, password=password,
+            )
+
+    def test_create_new_super_user(self):
+        """ Test creating a new superuser """
+        email = "test@PYTHONaccounting.com"
+        password = "Testpass123"
+        user = get_user_model().objects.create_super_user(email=email, password=password,)
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
